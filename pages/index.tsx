29fae98b7d78a2adfb557a9ef2b9cfe6994957aa
perrrek <danieljamesperek@gmail.com>
2025-04-22 17:09:19 +1000

    Add starter site

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen text-black p-10 bg-white/60">
+      <h1 className="text-4xl font-bold mb-4">Yggdrasil: Roots of Fate</h1>
+      <p className="text-lg">Welcome to the living codex of your Norse-inspired D&D world.</p>
+    </div>
+  );
+}
